0dd1983cda394e044c401d3d7b06cea77e0d5138
Piotr Schodziński <schodziak@gmail.com>
2020-04-13 13:57:27 +0200

    Updated application.properties, set configuration for url path.

---
+++ src/main/java/pl/wujekscho/beer/generic/config/RestApplication.java
@@ -0,0 +1,8 @@
+package pl.wujekscho.beer.generic.config;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("api")
+public class RestApplication extends Application {
+}
